128f56ec133ea9c7c8ff46592ab4385d3092152e
Paul Buser <buser.paul@gmail.com>
2022-06-30 01:44:40 -0400

    Refactor mat code into class.

--- Software/Example_HealthMatReader.py
+++ Software/Example_HealthMatReader.py
@@ -1,93 +1,77 @@
-import numpy as np
-import matplotlib as plt
-import serial
+# Standard libraries
 import sys
 import time
 
-plt.style.use('_mpl-gallery-nogrid')
-
-# This connects to serial
-ser = serial.Serial(
-    #You might change the PORT corresponding to the assigned by Operative System
-    '/dev/cu.usbmodem104742601',  # raspberry: '/dev/ttyUSB1'
-    baudrate=115200,
-    timeout=0.1)
+# Third-party libraries
+import numpy as np
+import matplotlib as plt
+import serial
 
 
-# Default parameters
+# This is how serial devices appear on Mac.
+# For other operating systems, you may need to change this.
+DEFAULT_DEVICE = '/dev/cu.usbmodem104742601'
 ROWS = 48  # Rows of the sensor
 COLS = 48  # Columns of the sensor
 
-# Variable declaration
-Values = np.zeros((ROWS, COLS))
 
+class Mat:
+    def __init__(self, port):
+        self.ser = serial.Serial(
+            port,
+            baudrate=115200,
+            timeout=0.1)
 
-def RequestPressureMap():
-    data = "R"
-    ser.write(data.encode())
+    def request_pressure_data(self):
+        data = 'R'
+        self.ser.write(data.encode())
 
+    def read_pressure_data(self):
+        matrix = np.zeros((ROWS, COLS), dtype=int)
+        _ = self.ser.read().decode('utf-8')
+        high_byte = self.ser.read()
+        low_byte = self.ser.read()
+        high = int.from_bytes(high_byte, 'big')
+        low = int.from_bytes(low_byte, 'big')
+        num_points = ((high << 8) | low)
+        _ = self.ser.read().decode('utf-8')
+        _ = self.ser.read().decode('utf-8')
+        x = y = n = 0
+        while n < num_points:
+            x = self.ser.read()
+            y = self.ser.read()
+            x = int.from_bytes(x, 'big')
+            y = int.from_bytes(y, 'big')
+            high_byte = self.ser.read()
+            low_byte = self.ser.read()
+            high = int.from_bytes(high_byte, 'big')
+            low = int.from_bytes(low_byte, 'big')
+            val = ((high << 8) | low)
+            matrix[y][x] = val
+            n += 1
+        return matrix
 
-def activePointsReceiveMap():
-    global Values
-    matrix = np.zeros((ROWS, COLS), dtype=int)
+    def get_pressure_data(self):
+        xbyte = ''
+        if self.ser.in_waiting > 0:
+            xbyte = self.ser.read().decode('utf-8')
+            if(xbyte == 'N'):
+                return self.read_pressure_map()
+            else:
+                self.ser.flush()
 
-    xbyte = ser.read().decode('utf-8')
+    def get_pressure_map(self):
+        self.request_pressure_data()
+        return self.get_pressure_data()
 
-    HighByte = ser.read()
-    LowByte = ser.read()
-    high = int.from_bytes(HighByte, 'big')
-    low = int.from_bytes(LowByte, 'big')
-    nPoints = ((high << 8) | low)
+    def print_matrix(self, data):
+        for i in range(COLS):
+            tmp = ''
+            for j in range(ROWS):
+                tmp = tmp + hex(int(data[i][j]))[-1]
+            print(tmp)
+        print('\n')
 
-    xbyte = ser.read().decode('utf-8')
-    xbyte = ser.read().decode('utf-8')
-    x = 0
-    y = 0
-    n = 0
-    while(n < nPoints):
-        x = ser.read()
-        y = ser.read()
-        x = int.from_bytes(x, 'big')
-        y = int.from_bytes(y, 'big')
-        HighByte = ser.read()
-        LowByte = ser.read()
-        high = int.from_bytes(HighByte, 'big')
-        low = int.from_bytes(LowByte, 'big')
-        val = ((high << 8) | low)
-        matrix[y][x] = val
-        n += 1
-    Values = matrix
- 
-def activePointsGetMap():
-    xbyte = ''
-    if ser.in_waiting > 0:
-        try:
-            xbyte = ser.read().decode('utf-8')
-        except Exception:
-            print("Exception")
-        if(xbyte == 'N'):
-            activePointsReceiveMap()
-        else:
-            ser.flush()
-
-class Null:
-    def write(self, text):
-        pass
-
-    def flush(self):
-        pass
-
-def getMatrix():
-    RequestPressureMap()
-    activePointsGetMap()
-
-def printMatrix():
-    for i in range(COLS):
-        tmp = ""
-        for j in range(ROWS):
-            tmp = tmp +   hex(int(Values[i][j]))[-1]
-        print(tmp)
-    print("\n")
 
 def generatePlot(Z):
     fig, ax = plt.subplots()
@@ -95,14 +79,14 @@ def generatePlot(Z):
     ax.contourf(Z, levels=np.linspace(Z.min(), Z.max(), 7))
 
     plt.show()
-   
-#Main
 
 
-while True:
-    getMatrix() # This function requests and parses a pressure map in the variable Values    
-    printMatrix()
-    generatePlot(Values)
-    # time.sleep(1)
+def main():
+    mat = Mat(DEFAULT_DEVICE)
+    while True:
+        data = mat.get_pressure_map()
+        mat.print_matrix(data)
 
 
+if __name__ == '__main__':
+    main()
